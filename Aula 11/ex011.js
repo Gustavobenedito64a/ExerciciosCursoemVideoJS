305bcaeb55ac1935b14af318c620c767e43fc1ef
Gustavo Benedito <gustavobeneditodeveloper@gmail.com>
2021-02-09 18:35:36 -0300

    curso em video

---
+++ Aula 11/ex011.js
@@ -0,0 +1,11 @@
+var velocidade = 50.5
+
+console.log('A velocidade do seu carro é de ' + velocidade + ' km/h');
+console.log("Dirija sempre utilizando cinto de segurança");
+
+if(velocidade > 60){
+    console.log("voce passou a velocidade permitida");
+}
+else{
+console.log("continue dirigindo assim")
+}
---
+++ Aula 12/ex013.js
@@ -0,0 +1,15 @@
+var idade = 88
+if(idade < 18){
+    console.log("Não é obrigado a vota")
+}else{
+    if(idade >=16 && idade < 18){
+    console.log("o Voto é opcional")
+}
+if(idade > 18 && idade < 65){
+    console.log("o Voto é obrigatorio")
+}
+if(idade >=65){
+    console.log("o Voto é opcional")
+}
+
+}
